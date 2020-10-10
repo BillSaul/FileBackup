5182753fdbf585bfd8eea150779c1b9865b2d546
不会写shell的小白 <47105171+BillSaul@users.noreply.github.com>
2020-10-10 14:28:30 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace PhotoBackup
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(109, 30);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(281, 25);
+			this.textBox1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 33);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(97, 15);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "目标文件夹：";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 83);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(67, 15);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "备份至：";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(109, 80);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.Size = new System.Drawing.Size(281, 25);
+			this.textBox2.TabIndex = 3;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(396, 30);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(68, 25);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "选择";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(396, 80);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(68, 25);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "选择";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox1.Controls.Add(this.listBox1);
+			this.groupBox1.Location = new System.Drawing.Point(534, 28);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(730, 538);
+			this.groupBox1.TabIndex = 6;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "日志";
+			// 
+			// listBox1
+			// 
+			this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.listBox1.CausesValidation = false;
+			this.listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 16;
+			this.listBox1.Location = new System.Drawing.Point(6, 24);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(718, 516);
+			this.listBox1.TabIndex = 0;
+			this.listBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox1_DrawItem);
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.label1);
+			this.groupBox2.Controls.Add(this.textBox1);
+			this.groupBox2.Controls.Add(this.button2);
+			this.groupBox2.Controls.Add(this.label2);
+			this.groupBox2.Controls.Add(this.button1);
+			this.groupBox2.Controls.Add(this.textBox2);
+			this.groupBox2.Location = new System.Drawing.Point(12, 28);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(516, 134);
+			this.groupBox2.TabIndex = 7;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "文件选择";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(79, 69);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(348, 49);
+			this.button3.TabIndex = 8;
+			this.button3.Text = "开始校验文件";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.button6);
+			this.groupBox3.Controls.Add(this.button4);
+			this.groupBox3.Controls.Add(this.button3);
+			this.groupBox3.Location = new System.Drawing.Point(12, 180);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(516, 343);
+			this.groupBox3.TabIndex = 9;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "操作";
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(79, 257);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(348, 49);
+			this.button6.TabIndex = 8;
+			this.button6.Text = "重置";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(79, 163);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(348, 49);
+			this.button4.TabIndex = 8;
+			this.button4.Text = "开始同步备份";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(12, 529);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(516, 36);
+			this.progressBar1.TabIndex = 10;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1276, 578);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "文件备份";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.groupBox3.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Button button6;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PhotoBackup
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		string photoPath;
+		string backupPath;
+		string TitleText = "欢迎使用！    +++ 软件版本v2.4 +++";
+		List<string> list1 = new List<string>();  //存放需要备份的
+		List<string> list2 = new List<string>();  //存放备份至的
+		List<string> list3 = new List<string>();  //存放需要备份的文件名
+		delegate void MyDelegate(string str);  //委托 日志输出
+		delegate void ButtonDelegate();  //委托 按钮控制
+		delegate void ProgressBarDelegate(int flag, int Count);  //委托 进度条
+		private MyDelegate myDeleteage;
+		private ButtonDelegate buttonDelegate;
+		private ProgressBarDelegate progressBarDelegate;
+		private Thread thread;
+
+		/*******************************方法*******************************/
+		public string Time()  //获取时间，用于日志输出
+		{
+			return DateTime.Now.ToString("[" + "HH:mm:ss" + "]");
+		}
+
+		public void logshow(string str)  //listBox自动到底部
+		{
+			bool scroll = false;
+			if (this.listBox1.TopIndex == this.listBox1.Items.Count - (int)(this.listBox1.Height / this.listBox1.ItemHeight))
+				scroll = true;
+			this.listBox1.Items.Add(Time() + str);
+			if (scroll)
+				this.listBox1.TopIndex = this.listBox1.Items.Count - (int)(this.listBox1.Height / this.listBox1.ItemHeight);
+		}
+
+		public void ButtonSwitch()
+		{
+			button3.Enabled = true;
+			button4.Enabled = true;
+		}
+
+		public void ProgressBarShow(int flag, int Count)
+		{
+			if (flag == 1)
+				progressBar1.PerformStep();
+			if (flag == 0)
+			{
+				progressBar1.Minimum = 1;  //最小值
+				progressBar1.Maximum = Count;  //最大值
+				progressBar1.Value = 1;  //初始值
+				progressBar1.Step = 1;
+			}
+		}
+
+		public string GetFileMD5(string name)  //获取文件MD5值
+		{
+			FileStream file2 = new FileStream(name, FileMode.Open);
+			MD5 md5 = new MD5CryptoServiceProvider();
+			byte[] retval = md5.ComputeHash(file2);
+			file2.Close();
+			StringBuilder sc = new StringBuilder();
+			for (int i = 0; i < retval.Length; i++)
+			{
+				sc.Append(retval[i].ToString("x2"));
+			}
+			return sc.ToString();
+		}
+
+		private void VerifyFile()  //检验文件
+		{
+			var fileName1 = Directory.GetFiles(@photoPath);
+			var fileName2 = Directory.GetFiles(@backupPath);
+			var Count = fileName1.Length + fileName2.Length;
+			this.Invoke(progressBarDelegate, 0, Count);
+			foreach (var name in fileName1)  //获取需要备份的文件夹下的文件名和大小
+			{
+				string md5 = GetFileMD5(name);
+				this.Invoke(myDeleteage, Path.GetFileName(name) + " ├─→ " + md5);
+				list1.Add(Path.GetFileName(name) + " ├─→ " + md5);
+				this.Invoke(progressBarDelegate, 1, Count);
+			}
+			this.Invoke(myDeleteage, "+++++++++++++++我是分割线+++++++++++++++");
+			foreach (var name in fileName2)  //获取需要备份至的文件夹下的文件名和大小
+			{
+				string md5 = GetFileMD5(name);
+				this.Invoke(myDeleteage, Path.GetFileName(name) + " ├─→ " + md5);
+				list2.Add(Path.GetFileName(name) + " ├─→ " + md5);
+				this.Invoke(progressBarDelegate, 1, Count);
+			}
+			this.Invoke(myDeleteage, "+++++++++++++++目标文件夹共计" + fileName1.Length + "个文件+++++++++++++++");
+			this.Invoke(myDeleteage, "+++++++++++++++备份文件夹共计" + fileName2.Length + "个文件+++++++++++++++");
+			list3 = list1.Except(list2).ToList();  //获取差值
+			foreach (var name in list3)  //输出未同步的文件
+			{
+				this.Invoke(myDeleteage, "未同步文件：" + name);
+			}
+			if (list3.Count == 0)
+			{
+				this.Invoke(myDeleteage, "***************没有需要同步的文件***************");
+			}
+			else
+				this.Invoke(myDeleteage, "+++++目标文件夹" + fileName1.Length + "个文件，备份文件夹" + fileName2.Length + "个文件，需要备份" + list3.Count + "个文件+++++");
+			this.Invoke(buttonDelegate);
+		}
+
+		private void BackupFile()
+		{
+			var Count = list3.Count;
+			this.Invoke(progressBarDelegate, 0, Count);
+			foreach (var name in list3)
+			{
+				string filePath1 = photoPath + "\\" + name.Split('├')[0];
+				string filePath2 = backupPath + "\\" + name.Split('├')[0];
+				File.Copy(filePath1, filePath2, true);  //复制文件
+				this.Invoke(myDeleteage, "已复制：" + name.Split('├')[0]);
+				this.Invoke(progressBarDelegate, 1, Count);
+			}
+			this.Invoke(myDeleteage, "+++++++++++++++文件备份完毕！+++++++++++++++");
+		}
+
+		/*******************************事件*******************************/
+		private void listBox1_DrawItem(object sender, DrawItemEventArgs e)  //改变listBox显示字体颜色
+		{
+			e.DrawBackground();
+			if (listBox1.Items[e.Index].ToString().IndexOf("*") != -1)  //如果带*号，则红字显示
+			{
+				e.Graphics.DrawString(((ListBox)sender).Items[e.Index].ToString(), e.Font, new SolidBrush(Color.Red), e.Bounds);
+			}
+			else if (listBox1.Items[e.Index].ToString().IndexOf("+") != -1)  //带+号，显示蓝色
+			{
+				e.Graphics.DrawString(((ListBox)sender).Items[e.Index].ToString(), e.Font, new SolidBrush(Color.Blue), e.Bounds);
+			}
+			else
+			{
+				e.Graphics.DrawString(((ListBox)sender).Items[e.Index].ToString(), e.Font, new SolidBrush(e.ForeColor), e.Bounds);
+			}
+			e.DrawFocusRectangle();
+		}
+
+		private void button1_Click(object sender, EventArgs e)  //选择需要备份的文件夹目录
+		{
+			FolderBrowserDialog dialog = new FolderBrowserDialog();
+			dialog.Description = "请选择文件夹";
+			if (dialog.ShowDialog() == DialogResult.OK)
+			{
+				photoPath = dialog.SelectedPath;
+				textBox1.Text = photoPath;
+				logshow("目标文件夹：" + photoPath);
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)  //选择需要备份到的文件夹
+		{
+			FolderBrowserDialog dialog = new FolderBrowserDialog();
+			dialog.Description = "请选择文件夹";
+			if (dialog.ShowDialog() == DialogResult.OK)
+			{
+				backupPath = dialog.SelectedPath;
+				textBox2.Text = backupPath;
+				logshow("备份至：" + backupPath);
+			}
+		}
+
+		private void button3_Click(object sender, EventArgs e)  //校验文件
+		{
+			if (photoPath == null || backupPath == null)
+			{
+				logshow("**********未选择文件路径**********");
+				MessageBox.Show("请选择文件路径！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+			else
+			{
+				button3.Enabled = false;
+				button4.Enabled = false;
+				if (thread == null)
+				{
+					thread = new Thread(VerifyFile);
+					thread.Start();
+				}
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)  //界面加载事件
+		{
+			logshow(TitleText);
+			myDeleteage = logshow;
+			buttonDelegate = ButtonSwitch;
+			progressBarDelegate = ProgressBarShow;
+		}
+
+		private void button4_Click(object sender, EventArgs e)  //开始同步
+		{
+			if (photoPath == null || backupPath == null)
+			{
+				logshow("**********未选择文件路径**********");
+				MessageBox.Show("请选择文件路径！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+			else
+			{
+				if (list3.Count == 0)
+				{
+					logshow("**********未检验文件**********");
+					MessageBox.Show("请先校验文件！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				}
+				else
+				{
+					if (thread == null)
+					{
+						thread = new Thread(BackupFile);
+						thread.Start();
+					}
+					else
+					{
+						thread.Abort();
+						thread = new Thread(BackupFile);
+						thread.Start();
+					}
+				}
+			}
+		}
+
+		private void button6_Click(object sender, EventArgs e)  //重置
+		{
+			listBox1.Items.Clear();
+			logshow(TitleText);
+			list1.Clear();
+			list2.Clear();
+			list3.Clear();
+			photoPath = null;
+			backupPath = null;
+			textBox1.Text = null;
+			textBox2.Text = null;
+			button3.Enabled = true;
+			button4.Enabled = true;
+			progressBar1.Value = 1;
+			if (thread != null)
+				thread.Abort();
+		}
+	}
+}
